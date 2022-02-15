dec0783498a943a435aada00b3ec603e7e47f60a
Allisona1012 <allisona1012@gmail.com>
2022-02-14 21:38:04 -0600

    registration form made

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
+
 from app import app 
-from flask import render_template,url_for
+from flask import render_template,url_for,flash
+
+from app.forms import LoginForm, RegistrationForm
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', form = form)
+
+@app.route('/login',methods=['GET','POST'] )
+def login():
+    form = LoginForm()
+    return render_template('login.html', form = form)
+
